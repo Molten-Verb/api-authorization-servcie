09835f88127fcef0031d5466472dace7b06048de
Molten-Verb <sanches-9868@yandex.ru>
2024-12-03 15:38:59 +0300

    модели

--- database/migrations/2024_12_03_115039_create_invitations_table.php
+++ database/migrations/2024_12_03_115039_create_invitations_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('invitations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email');
             $table->string('token');
             $table->timestamps();
         });
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('invitations');
     }
 };
