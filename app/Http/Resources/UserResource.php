cbd6dd5b615e21d13592eedbfea06a82a33c8459
Molten-Verb <sanches-9868@yandex.ru>
2024-12-05 23:29:03 +0300

    исправлена структура ответа

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -6,23 +6,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    protected $token;
-
-    public function __construct($resource, $token = null)
-    {
-        parent::__construct($resource);
-        $this->token = $token;
-    }
-
     public function toArray($request)
     {
         return [
             'status' => 200,
-            'token' => $this->token,
-            'company' => [
-                'name' => $this->companies->pluck('name'),
-                'roles' => $this->roles->pluck('name'),
-            ],
+            'companies' => $this->companies->map(function ($company) {
+                $roles = $company->roles->pluck('name');
+                return [
+                    'name' => $company->name,
+                    'roles' => $roles,
+                ];
+            }),
         ];
     }
 }
